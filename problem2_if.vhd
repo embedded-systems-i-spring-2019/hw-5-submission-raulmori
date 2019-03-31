55d53fe04dcf6de20472d54e7710682b2fc6bdbb
raulmori <47406865+raulmori@users.noreply.github.com>
2019-03-31 11:48:47 -0400

    Update problem2_if.vhd

--- problem2_if.vhd
+++ problem2_if.vhd
@@ -21,42 +21,35 @@ end problem2_if;
 
 architecture Behavioral of problem2_if is
 
---intermediate signals
-signal A_out, B_out, C_out : std_logic;
+    signal A_out, B_out, C_out : std_logic;       --intermediate signals
 
-begin
-
-process(A, B, D)
     begin
-    
-    --A out
-    if((A(0) AND A(1)) = '1') then
-        A_out <= '1';
-    else
-        A_out <= '0';
-    end if;
-    --B_out
-    if((B(0) OR B(1)) = '1') then
-        B_out <= '1';
-    else
-        B_out <= '0';
-    end if;
-    --C_out
-    if((B(1) AND (not D)) = '1') then
-        C_out <= '1';
-    else
-        C_out <= '0';
-    end if;
-    
-    --E_out
-    if((A_out OR B_out OR C_out) = '1') then
-        E_out <= '1';
-    else
-        E_out <= '0';
-    end if;
-        
-    
-        
-end process;
+        process(A, B, D)          --Notice that for the entire circuit we use only ONE +process
+              begin
+                
+              if((A(0) AND A(1)) = '1') then      ----A out. The beauty of VHDL is that it lets us skip trust tables. The system assumes we have an AND gate
+                   A_out <= '1';
+              else
+                   A_out <= '0';
+              end if;
 
+              if((B(0) OR B(1)) = '1') then       --B_out
+                    B_out <= '1';
+              else
+                    B_out <= '0';
+              end if;
+
+              if((B(1) AND (not D)) = '1') then     --C_out
+                   C_out <= '1';
+              else
+                    C_out <= '0';
+              end if;
+
+
+              if((A_out OR B_out OR C_out) = '1') then      --E_out
+                   E_out <= '1';
+              else
+                    E_out <= '0';
+              end if;
+        end process;
 end Behavioral;
