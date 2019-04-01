ac91cce23d403eb9f69997d9e7fa3b06e0019f82
raulmori <47406865+raulmori@users.noreply.github.com>
2019-04-01 01:56:41 -0400

    Update Problem7(If).vhd

--- Problem7(If).vhd
+++ Problem7(If).vhd
@@ -11,38 +11,34 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity problem7_if is
-  Port (input : in std_logic_vector(2 downto 0);
-        output: out std_logic_vector(7 downto 0));
+        Port (
+              input : in std_logic_vector(2 downto 0);
+              output: out std_logic_vector(7 downto 0));
 end problem7_if;
 
+  
 architecture Behavioral of problem7_if is
-
-begin
-
-
-    --process
-    process(input)
         begin
-            if(input = "000") then
-                output <= "01111111";
-            elsif(input = "001") then
-                output <= "10111111";
-            elsif(input = "010") then
-                output <= "11011111";
-            elsif(input = "011") then
-                output <= "11101111";
-            elsif(input = "100") then
-                output <= "11110111";
-            elsif(input = "101") then
-                output <= "11111011";
-            elsif(input = "110") then
-                output <= "11111101";
-            elsif(input = "111") then
-                output <= "11111110";
-            else
-                output <= "11111111";
-            end if;
-    end process;
-
-
+              process(input)            --process
+                      begin
+                            if(input = "000") then
+                                      output <= "01111111";
+                            elsif(input = "001") then
+                                      output <= "10111111";   
+                            elsif(input = "010") then
+                                      output <= "11011111";
+                            elsif(input = "011") then
+                                      output <= "11101111";
+                            elsif(input = "100") then
+                                      output <= "11110111";
+                            elsif(input = "101") then
+                                      output <= "11111011";
+                            elsif(input = "110") then
+                                      output <= "11111101";
+                            elsif(input = "111") then
+                                    output <= "11111110";
+                            else
+                                   output <= "11111111";
+                            end if;
+              end process;
 end Behavioral;
