8e1500f9085db51fc5dfcda4ee9bdb9097191777
raulmori <47406865+raulmori@users.noreply.github.com>
2019-04-14 03:23:09 -0400

    Update Problem6(Case).vhd

--- Problem6(Case).vhd
+++ Problem6(Case).vhd
@@ -1,5 +1,5 @@
 
-In here we use the case method
+--In here we use the case method
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -14,26 +14,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity problem6_case is
-  Port (input : in std_logic_vector(7 downto 0);
-        sel : in std_logic_vector(2 downto 0);
-        output : out std_logic );
+      Port (
+            DATA_in : in std_logic_vector(7 downto 0);   --This is the 8-bit INPUT bundle
+            SEL : in std_logic_vector(2 downto 0);      --This is dummy variable we creat to control the OPTIONS
+            Output : out std_logic );
 end problem6_case;
 
 architecture Behavioral of problem6_case is
 
       begin
-            process(input, sel)           --process
+            process(DATA_in, SEL)           --process
                   begin
-                      case(sel) is                            --This is the "case method" of PROCESS-STATEMENT
-                              when "000" => output <= input(0);
-                              when "001" => output <= input(1);
-                              when "010" => output <= input(2);
-                              when "011" => output <= input(3);
-                              when "100" => output <= input(4);
-                              when "101" => output <= input(5);
-                              when "110" => output <= input(6);
-                              when "111" => output <= input(7);     --Notice we have 8 bits
-                              when others => output <= '0';        --The last outputs a "0" to eliminate bugs
+                      case(SEL) is                            --This is the "case method" of PROCESS-STATEMENT
+                              when "000" => 
+                                    Output <= DATA_in(0);
+                              when "001" => 
+                                    Output <= DATA_in(1);
+                              when "010" => 
+                                    Output <= DATA_in(2);
+                              when "011" => 
+                                    Output <= DATA_in(3);
+                              when "100" => 
+                                    Output <= DATA_in(4);
+                              when "101" => 
+                                    Output <= DATA_in(5);
+                              when "110" => 
+                                    Output <= DATA_in(6);
+                              when "111" => 
+                                    Output <= DATA_in(7);     --Notice we have 8 bits
+                              when others =>
+                                    Output <= '0';        --The last outputs a "0" to eliminate bugs
                           end case;
             end process;
 end Behavioral;
